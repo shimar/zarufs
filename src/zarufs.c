0cf6635d756a01a71ed2fa2195d0c9c929cdf2f8
shimar <shima01dev@gmail.com>
2015-10-16 19:31:21 +0900

    add mounting function.

--- src/zarufs.c
+++ src/zarufs.c
@@ -1,16 +1,40 @@
 /* zarufs.c */
 #include <linux/module.h>
+#include <linux/fs.h>
 
 #include "../include/zarufs.h"
 #include "zarufs_utils.h"
 
+static struct dentry *zarufs_mount(struct file_system_type *fs_type,
+                                   int flags,
+                                   const char *dev_name,
+                                   void *data);
+
+
+static struct file_system_type zarufs_fstype = {
+  .name     = "zarufs",
+  .mount    = zarufs_mount,
+  .fs_flags = FS_REQUIRES_DEV,
+};
+
+
+static struct dentry *zarufs_mount(struct file_system_type *fs_type,
+                                   int flags,
+                                   const char *dev_name,
+                                   void *data) {
+
+  DBGPRINT("[ZARUFS] Not Implemented Yet!\n");
+  return NULL;
+}
+
 static int __init init_zarufs(void) {
   DBGPRINT("[ZARUFS] Hello, World.\n");
-  return 0;
+  return register_filesystem(&zarufs_fstype);
 }
 
 static void __exit exit_zarufs(void) {
   DBGPRINT("[ZARUFS] GoodBye!.\n");
+  unregister_filesystem(&zarufs_fstype);
   return;
 }
 
