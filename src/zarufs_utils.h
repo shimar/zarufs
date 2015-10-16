b55f7e1d98974a036fc7785a8d109b9d16b878a9
shimar <shima01dev@gmail.com>
2015-10-16 19:15:06 +0900

    add macros for debug.

--- src/zarufs_utils.h
+++ src/zarufs_utils.h
@@ -1,3 +1,19 @@
 #ifndef _ZARUFS_UTILS_H_
 #define _ZARUFS_UTILS_H_
+
+#define ZARUFS_DEBUG
+
+#ifdef ZARUFS_DEBUG
+#define DBGPRINT(msg, args...) do { \
+    printk(KERN_INFO msg, ##args); \
+  } while(0)
+#else
+#define DBGPRINT(msg, args...) do {} while (0)
+#endif
+
+#define ZARUFS_ERROR(msg, args...) do {         \
+    printk(KERN_ERR msg, ##args);              \
+} while(0)
+
+
 #endif //_ZARUFS_UTILS_H_
