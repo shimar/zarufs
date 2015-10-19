b2c6d00f6fec844acd2731f9ee6d91abb8156bb8
shimar <shima01dev@gmail.com>
2015-10-19 21:21:33 +0900

    add some defines.

--- include/zarufs.h
+++ include/zarufs.h
@@ -11,6 +11,83 @@
 #define ZARUFS_EXT2_BL_INO       5
 #define ZARUFS_EXT2_UNDER_DIR_NO 6
 
+/* defines for s_state. */
+#define EXT2_VALID_FS (1)
+#define EXT2_ERROR_FS (2)
+
+/* defines for s_errors. */
+#define EXT2_ERRORS_CONTINUE (1)
+#define EXT2_ERRORS_RO       (2)
+#define EXT2_ERRORS_PANIC    (3)
+
+/* defines for s_creator_os. */
+#define EXT2_OS_LINUX   (0)
+#define EXT2_OS_HURD    (1)
+#define EXT2_OS_MASIX   (2)
+#define EXT2_OS_FREEBSD (3)
+#define EXT2_OS_LITES   (4)
+
+/* defines for s_rev_level. */
+#define EXT2_GOOD_OLD_REV (0)
+#define EXT2_DYNAMIC_REV  (1)
+
+/* defines for s_def_resuid. */
+#define EXT2_DEF_RESUID   (0)
+/* defines for s_def_resgid. */
+#define EXT2_DEF_RESGID   (0)
+
+
+/* defines for s_feature_compat. */
+#define EXT2_FEATURE_COMPAT_DIR_PREALLOC (0x0001)
+#define EXT2_FEATURE_COMPAT_IMAGC_INODES (0x0002)
+#define EXT2_FEATURE_COMPAT_HAS_JOURNAL  (0x0004)
+#define EXT2_FEATURE_COMPAT_EXT_ATTR     (0x0008)
+#define EXT2_FEATURE_COMPAT_RESIZE_INO   (0x0010)
+#define EXT2_FEATURE_COMPAT_DIR_INDEX    (0x0012)
+
+#define EXT2_FEATURE_COMPAT_SUPP         ZARUFS_FEATURE_COMPAT_EXT_ATTR
+
+/* defines for s_feature_ro_compat. */
+#define EXT2_FEATURE_RO_COMPAT_SPARSE_SUPER (0x0001)
+#define EXT2_FEATURE_RO_COMPAT_LARGE_FILE   (0x0002)
+#define EXT2_FEATURE_RO_COMPAT_BTREE_DIR    (0x0004)
+#define EXT2_FEATURE_RO_COMPAT_ANY          (0xFFFFFFFF)
+
+#define EZT2_FEATURE_RO_COMPAT_SUPP (EXT2_FEATURE_RO_COMPAT_SPARSE_SUPER | \
+                                     EXT2_FEATURE_RO_COMPAT_LARGE_FILE   | \
+                                     EXT2_FEATURE_RO_COMPAT_BTREE_DIR)
+#define EXT2_FEATURE_RO_COMPAT_UNSUPPORTED ~EXT2_FEATURE_RO_COMPAT_SUPP
+
+/* defines for s_feature_icompat. */
+#define EXT2_FEATURE_INCOMPAT_COMPRESSION (0x0001)
+#define EXT2_FEATURE_INCOMPAT_FILETYPE    (0x0002)
+#define EXT2_FEATURE_INCOMPAT_RECOVER     (0x0004)
+#define EXT2_FEATURE_INCOMPAT_JOURNAL_DEV (0x0008)
+#define EXT2_FEATURE_INCOMPAT_META_BG     (0x0010)
+
+#define EXT2_FEATURE_INCOMPAT_SUPP (EXT2_FEATURE_INCOMPAT_FILETYPE | \
+                                    EXT2_FEATURE_INCOMPAT_META_BG)
+#define EXT2_FEATURE_INCOMPAT_UNSUPPORTED ~EXT2_FEATURE_INCOMPAT_SUPP
+
+
+/* defines for s_default_mount_opts and s_mount_opts. */
+#define EXT2_MOUNT_CHECK        (0x00000001)
+#define EXT2_MOUNT_OLDALLOC     (0x00000002)
+#define EXT2_MOUNT_GRPID        (0x00000004)
+#define EXT2_MOUNT_DEBUG        (0x00000008)
+#define EXT2_MOUNT_ERRORS_CONT  (0x00000010)
+#define EXT2_MOUNT_ERRORS_RO    (0x00000020)
+#define EXT2_MOUNT_ERRORS_PANIC (0x00000040)
+#define EXT2_MOUNT_MINIX_DF     (0x00000080)
+#define EXT2_MOUNT_NOBH         (0x00000100)
+#define EXT2_MOUNT_NO_UID32     (0x00000200)
+#define EXT2_MOUNT_XATTR_USER   (0x00000400)
+#define EXT2_MOUNT_POSIX_ACL    (0x00000800)
+#define EXT2_MOUNT_XIP          (0x00001000)
+#define EXT2_MOUNT_USRQUOTA     (0x00002000)
+#define EXT2_MOUNT_GRPQUOTA     (0x00004000)
+#define EXT2_MOUNT_RESERVATION  (0x00008000)
+
 struct zarufs_super_block {
   __le32 s_inodes_count;
   __le32 s_blocks_count;
@@ -74,8 +151,38 @@ struct zarufs_super_block {
 };
 
 struct zarufs_sb_info {
+  /* buffer cache infomations. */
   struct zarufs_super_block *s_zsb;
-  struct buffer_head      *s_sbh;
+  struct buffer_head        *s_sbh;
+  struct buffer_head        **s_group_desc;
+
+  /* disk information cache. */
+  // super block.
+  unsigned long  s_sb_block;
+  unsigned long  s_mount_opt;
+  unsigned short s_mount_state;
+
+  // inode.
+  unsigned short s_inode_size;
+  unsigned int   s_first_ino;
+  unsigned long  s_inodes_per_group;
+  unsigned long  s_inodes_per_block;
+  unsigned long  s_itb_per_group;
+
+  // group.
+  unsigned long  s_groups_count;
+  unsigned long  s_blocks_per_group;
+  unsigned long  s_desc_per_block; /* # of group desc per block. */
+  unsigned long  s_gdb_count;      /* # of group desc blocks.    */
+
+  // fragment.
+  unsigned long  s_frag_size;
+  unsigned long  s_frags_per_block;
+  unsigned long  s_frags_per_group;
+
+  // defaults.
+  kuid_t         s_resuid;
+  kgid_t         s_resgid;
 };
 
 #endif /* _ZARUFS_H_ */
