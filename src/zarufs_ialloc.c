0a09c6538942279ab75ac7c7016a08279c707a6a
shimar <shima01dev@gmail.com>
2015-11-17 12:10:20 +0900

    add ialloc.

---
+++ src/zarufs_ialloc.c
@@ -0,0 +1,4 @@
+#include "../include/zarufs.h"
+#include "zarufs_utils.h"
+#include "zarufs_inode.h"
+#include "zarufs_block.h"
---
+++ src/zarufs_ialloc.h
@@ -0,0 +1,4 @@
+#ifndef _ZARUFS_IALLOC_H_
+#define _ZARUFS_IALLOC_H_
+
+#endif _ZARUFS_IALLOC_H_
